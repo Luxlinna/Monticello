3cad7676e9fdb20b640b2ad9ba125a830639bd37
luxlinna <luxlinna@gmail.com>
2021-03-01 17:43:18 +0100

    commit all files

--- assets/Javascript/api.js
+++ assets/Javascript/api.js
@@ -1,4 +1,4 @@
-async
+/* async */
 defer
-src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+src = "https://maps.googleapis.com/maps/api/js?AIzaSyBCxvydxuohq-V2OxbPfms_6k2ZSmnQUWk&callback=initMap&libraries=&v=weekly"
 type = "text/javascript"
